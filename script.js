8954fd4ba6fb6fc87a7179a2097438344d3d5d54
Ibrahim <ibrooz94@gmail.com>
2021-07-29 00:38:38 +0100

    Alpha 1 Release

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
-	// Defining async function
+// Get form input (base.html)
+function valueSender(){
+	let x = document.getElementById("userinput").value;
+	const api_url = "https://polar-temple-04652.herokuapp.com/s/" + x;
+	localStorage.setItem("myValue", api_url);
+	window.location.href = "results.html";
+	}
+
+// Defining async function
 async function getapi(url) {
     // show loader
 	document.getElementById('loading').classList.remove('hidden');
@@ -18,24 +26,11 @@ async function getapi(url) {
 	}	
 }
 
-// Calling that async function
-document.getElementById('aform').addEventListener('submit', function(e){
-	e.preventDefault()
-	let x = document.getElementById("input").value;
-	const api_url = "https://polar-temple-04652.herokuapp.com/s/" + x;
-	getapi(api_url)
-})
-
-// Function 
-function hideloader() {
-	document.getElementById('loading').style.display = 'none';
-}
-
-// Function to define innerHTML for display
+// Function to define innerHTML for display [ RESULTS PAGE ]
 function show(data) {
 	let results =
 		`<div class="card-columns">`;
-	
+	let i = 0;
 	for (let r of data) {
 		results += `
 		<div class="card border-dark" >
@@ -43,20 +38,19 @@ function show(data) {
 			<div class="card-body">
 				<h5 class="card-title">${r.name}</h5>
 				<p class="card-text"> ${r.exr} </p>
-				<a class="btn btn-outline-success my-2 my-sm-0" id="download" href="${r.url}">GET LINK </a>
+				<a onclick="linkSender(event)" class="btn btn-outline-success my-2 my-sm-0" id="download-${i++}" data-record-id="${r.url}">GET LINK </a>
 			</form>
 			</div>
-
 		</div>
-		`;
-	}
+		`;}
 
 	// Setting innerHTML as tab variable
 	document.getElementById("cards").innerHTML = results;
+
 	let y = document.getElementById("download");
 	if (y){
 		console.log("we here")
-		check();
+
 	}
 
 }
@@ -79,18 +73,7 @@ async function getlink(url) {
 	present(data);
 }
 
-function check(){
-	document.getElementById('download').addEventListener('click', function(e){
-	e.preventDefault()
-	console.log("we here ")
-	let x = this.href;
-	console.log(x)
-	const link_url = "https://polar-temple-04652.herokuapp.com/d/" + x;
-	getlink(link_url)
-})
-}
-
-
+// Function to define innerHTML for display [DOWNLOAD PAGE ]
 function present(data) {
 	let tab =
 		`
@@ -110,4 +93,13 @@ function present(data) {
 
 	document.getElementById("cards").innerHTML = tab;
 }
-
+// download.html
+function linkSender(e) {
+	console.log("I worked")
+	e = e || window.event;
+	e.preventDefault();
+	let x = event.currentTarget.dataset.recordId;
+	const api_url = "https://polar-temple-04652.herokuapp.com/d/" + x;
+	localStorage.setItem("myLink", api_url);
+	window.location.href = "download.html";
+}
